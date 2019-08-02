49583beb05a27cfe1331c88be4347dc4a45d2fe6
e10ulen <slcb.sena@gmail.com>
2019-08-02 22:05:32 +0900

    fix localstorage

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -7,12 +7,13 @@
       <b-button v-on:click="addTodoItem()">Add</b-button>
       <b-button v-on:click="deleteTodoItem()">Delete</b-button>
     </b-form>
+    <p></p>
     <b-form v-for="item in items" v-bind:key="item.title">
+
       <label v-bind:class="{ done: isChecked }">
         <b-form-checkbox type="checkbox" class="mb-2 mr-sm-2 mb-sm-0" v-model="item.isChecked">{{ item.title }}</b-form-checkbox>
       </label>
     </b-form>
-
   </div>
 </template>
 
@@ -21,39 +22,41 @@ export default {
   props: {
     isChecked: Boolean,
     title: String
-
+  },
+  mounted: function () {
+    this.loadTodo()
+    // this.items = JSON.parse(localStorage.getItem('items')) || []
   },
   data: function () {
     return {
-      items: [
-        { title: '領収書を準備する', isChecked: true },
-        { title: 'Vue.jsハンズオンの資料を作る', isChecked: true },
-        { title: '参加者の人数を確認する', isChecked: false },
-        { title: 'ピザを注文する', isChecked: false },
-        { title: '参加費のお釣りを準備する', isChecked: false },
-        { title: '会場設営をする', isChecked: false }
-      ],
+      items: [],
       newTodoItem: ''
     }
   },
+  watch: {
+    items: {
+      handler: function () {
+        this.saveTodo()
+      },
+      deep: true
+    }
+  },
   methods: { // methodsオプションをまるっと追加
+
     addTodoItem: function () {
       this.items.push({
         title: this.newTodoItem,
         isChecked: false
       })
       this.newTodoItem = ''
-      this.saveTodo()
     },
     deleteTodoItem: function () {
       this.items = this.items.filter(function (item) {
         return item.isChecked === false
       })
-      this.saveTodo()
     },
     deleteTodoText: function () {
       this.newTodoItem = ''
-      this.saveTodo()
     },
     saveTodo: function () {
       localStorage.setItem('items', JSON.stringify(this.items))
@@ -63,9 +66,6 @@ export default {
       if (!this.items) {
         this.items = []
       }
-    },
-    mounted: function () {
-      this.loadTodo()
     }
   }
 
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLocalStorage from 'vue-ls'
 /* ここから */
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
+Vue.use(VueLocalStorage)
 /* ここまで */
 Vue.config.productionTip = false
 
@@ -17,5 +19,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-
 })
