9d166dabfbf43fde940fc1210a9a471868778d38
e10ulen <slcb.sena@gmail.com>
2019-08-05 11:01:50 +0900

    more... write

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,6 +17,15 @@
         How to Todo に使い方を記載
       </h2>
     </div>
+    <div class="container">
+      <h1 class="title">more...</h1>
+      <h2 class="subtitle">
+        Google Drive APIを用いて、Todoをローカルストレージではなく、Googleアカウント上での共有に切り替えて<br>
+        スマホ、PC、タブレットから同じTodoを参照できるようにする事。<br>
+        ローカルストレージの廃止、今使われている部分を刷新し、ローカルストレージのデータを一度でも拾ったら、ローカルストレージを削除して、<br>
+        ブラウザが保管する部分をやめていく（習作のためローカルストレージを使っただけであって、今後はTodoの個人アカウント内共有を目指す。<br>
+      </h2>
+    </div>
   </section>
 </template>
 <script>
